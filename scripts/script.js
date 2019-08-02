26f5b4fc9459f4163108014917877ad4844d7269
Nikos Koutelidis <nikoutel@gmail.com>
2019-08-02 09:00:44 +0200

    Set background image dynamicaly

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,21 @@
+const imageDirectory = 'img/';
+const backgroundImageDefault = imageDirectory + 'beautiful-branches.jpg';
+
 $(document).ready(function () {
     $('body').on('click', '[data-toggle="modal"]', function(){
         $($(this).data("target")+' .modal-title').text($(this).data("title"));
         $($(this).data("target")+' .modal-body').load($(this).data("remote"));
     });
 });
+
+function getBackgroundImage() {
+    let backgroundImage;
+    let backgroundImageStored = localStorage.getItem('backgroundImage');
+
+    if (backgroundImageStored == null) {
+        backgroundImage = backgroundImageDefault;
+    } else {
+        backgroundImage = imageDirectory + backgroundImageStored;
+    }
+    return backgroundImage;
+}
